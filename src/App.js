ffb30614738da1dac02fb67e556c840f8a3025b5
Josh <greeny1248@gmail.com>
2023-02-06 14:35:59 +0000

    Added getReviews, some simple css, added header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import { Routes, Route } from 'react-router-dom';
-import './App.css';
-import Header from './components/header';
-import { Reviews } from './components/reviews';
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./components/header";
+import { Reviews } from "./components/reviews";
 
 function App() {
   return (
     <div className="App">
-<Header />
-<Routes>
-        <Route path='/' element={<Reviews />}></Route>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Reviews />}></Route>
       </Routes>
     </div>
   );
